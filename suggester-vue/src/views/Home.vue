802e6135507b6ddab686f928a3705ae6ab30afa3
Turan Yunus <turan.celikel@optdcom.net>
2021-02-07 17:49:08 +0300

    first commit

---
+++ suggester-vue/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+
+  @media (--t) {
+    color: blue;
+  }
+}
+</style>
